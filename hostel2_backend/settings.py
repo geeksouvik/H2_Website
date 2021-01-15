7d9185366e430c4655033ac7761a43a21a939a5d
adityajain1152 <adityajain1152@gmail.com>
2021-01-15 23:48:30 +0530

    password reset implemented

--- hostel2_backend/settings.py
+++ hostel2_backend/settings.py
@@ -131,3 +131,11 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'home_page'
 LOGIN_URL = 'login'
+
+EMAIL_USE_TLS = True
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT =  587
+EMAIL_HOST_USER = 'thetriquetra01@gmail.com'
+EMAIL_HOST_PASSWORD = 'enigma*01'
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
--- hostel2_backend/urls.py
+++ hostel2_backend/urls.py
@@ -13,7 +13,19 @@ urlpatterns = [
     path('profile/', user_views.profile, name = 'profile'),
     path('profile_update/', user_views.profile_update, name = 'profile_update'),
     path('login/', user_views.loginPage, name = 'login'),
-    path('logout/', user_views.logoutPage, name = 'logout')
+    path('logout/', user_views.logoutPage, name = 'logout'),
+    path('password-reset/',
+         auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'),
+         name = 'password_reset'),
+    path('password-reset/done',
+         auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'),
+         name='password_reset_done'),
+    path('password-reset/confirm/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'),
+         name='password_reset_confirm'),
+    path('password-reset-complete/',
+         auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'),
+         name='password_reset_complete'),
 ]
 
 if settings.DEBUG:
