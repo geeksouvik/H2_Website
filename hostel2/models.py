5390d59f0a95c1514e9f7131bc1c068ec718ecdb
adityajain1152 <adityajain1152@gmail.com>
2021-01-18 00:45:03 +0530

    minor css changes and blog update

--- hostel2/models.py
+++ hostel2/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
+from ckeditor.fields import RichTextField
 
 
 class Updates(models.Model):
@@ -168,7 +169,7 @@ class AlumniTestimony(models.Model):
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
-    content = models.TextField()
+    content = RichTextField(blank=True, null=True)
     date_posted = models.DateTimeField(default = timezone.now)
     author = models.ForeignKey(User, on_delete = models.CASCADE)
 
--- hostel2_backend/settings.py
+++ hostel2_backend/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'hostel2.apps.Hostel2Config',
     'users.apps.UsersConfig',
     'crispy_forms',
+    'ckeditor',
     'django.contrib.admin',
     'django.contrib.auth',
     'bootstrap4',
